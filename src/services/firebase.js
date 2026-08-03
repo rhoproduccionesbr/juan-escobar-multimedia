ff286a2a896e7444d73dd3e5335ae2e82acf2e68
Rodrigo Ramirez <rhoproduccionesbr@gmail.com>
2026-08-03 16:34:06 -0300

    feat: initialize firebase, firestore, and auth services with environment configuration

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -3,13 +3,13 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-    appId: import.meta.env.VITE_FIREBASE_APP_ID,
-    measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY || "AIzaSyDckxi6pdWyXtDEfSebOuCEKzfRTi56aqQ",
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN || "serverccpyradio.firebaseapp.com",
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID || "serverccpyradio",
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET || "serverccpyradio.firebasestorage.app",
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID || "667905363599",
+    appId: import.meta.env.VITE_FIREBASE_APP_ID || "1:667905363599:web:8482fac36b6f3e45992afa",
+    measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID || "G-CN9SM7H53N"
 };
 
 const app = initializeApp(firebaseConfig);
